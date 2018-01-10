6f200c01eeb7709f94f85b7b03fcc87916e48de4
Sam Aparicio <sam@ringio.com>
2018-01-09 22:04:22 -0500

    MQTT
    This version doesn't really work but it outlines how the MQTT code will eventually work

--- receiver_esp8266_lorawing/receiver_esp8266_lorawing.ino
+++ receiver_esp8266_lorawing/receiver_esp8266_lorawing.ino
@@ -9,6 +9,10 @@
 #include <SPI.h>
 #include <RH_RF95.h>
 
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
 // for ESP w/featherwing 
 #define RFM95_CS  2    // "E"
 #define RFM95_RST 16   // "D"
@@ -23,6 +27,50 @@ RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 // Blinky on receipt 0 for Huzzah 13 for most other Feathers
 #define LED 0
+
+
+// Adafruit IO and Wi-Fi Access Point info
+#define WLAN_SSID       "your ssid here"
+#define WLAN_PASS       "your password here"
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "your Adafruit IO user name here"
+#define AIO_KEY         "your Adafruit IO private key here"
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+
+// Store the MQTT server, username, and password in flash memory.
+// This is required for using the Adafruit MQTT library.
+const char MQTT_SERVER[] PROGMEM    = AIO_SERVER;
+const char MQTT_USERNAME[] PROGMEM  = AIO_USERNAME;
+const char MQTT_PASSWORD[] PROGMEM  = AIO_KEY;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, AIO_SERVERPORT, MQTT_USERNAME, MQTT_PASSWORD);
+
+/****************************** Feeds ***************************************/
+
+// *** These are just examples! Change these to your own feed names. ***
+
+const char TEMP_FEED[] PROGMEM = AIO_USERNAME "/feeds/LoRaIOXtemp";
+Adafruit_MQTT_Publish tempfeed = Adafruit_MQTT_Publish(&mqtt, TEMP_FEED);
+
+const char BATT_FEED[] PROGMEM = AIO_USERNAME "/feeds/LoRaIOXbatt";
+Adafruit_MQTT_Publish battfeed = Adafruit_MQTT_Publish(&mqtt, BATT_FEED);
+
+const char TEMP_FEED2[] PROGMEM = AIO_USERNAME "/feeds/LoRaIOXtemp2";
+Adafruit_MQTT_Publish tempfeed2 = Adafruit_MQTT_Publish(&mqtt, TEMP_FEED2);
+
+const char BATT_FEED2[] PROGMEM = AIO_USERNAME "/feeds/LoRaIOXbatt2";
+Adafruit_MQTT_Publish battfeed2 = Adafruit_MQTT_Publish(&mqtt, BATT_FEED2);
+
+/****************************************************************************/
+
+// Counter to help keep Adafruit IO connection alive
+uint32_t connectCounter = 0;
 
 void setup()
 {
@@ -62,6 +110,25 @@ void setup()
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
+
+ // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  Serial.println("");
+
+  MQTT_connect();
+  
 }
 
 void loop()
@@ -86,6 +153,44 @@ void loop()
       //uint8_t data[] = "Yo back";
       //rf95.send(data, sizeof(data));
       rf95.send(buf, len);
+
+      /* MQTT publishing code goes here 
+       *  
+       *          
+       *          bool publishResults = true;
+
+        if (RFM_Package[2] == 0x20) // We check Node ID to determine which feeds to send the data to
+        { 
+          char pubTemp[7];
+          dtostrf(temp, 6, 2, pubTemp);   // Don't send raw doubles. They will not display correctly
+          if (!tempfeed.publish(pubTemp)) // when graphed in Adafruit IO (currently)
+            publishResults = false;
+          delay(10);
+          char pubBatt[6];
+          dtostrf(batt, 4, 2, pubBatt);
+          if (!battfeed.publish(pubBatt))
+            publishResults = false;
+          delay(10);
+        } else if (RFM_Package[2] == 0x21) { // Different node ID, upload to different feeds
+          char pubTemp[7];
+          dtostrf(temp, 6, 2, pubTemp);
+          if (!tempfeed2.publish(pubTemp))
+            publishResults = false;
+          delay(10);
+          char pubBatt[6];
+          dtostrf(batt, 4, 2, pubBatt);
+          if (!battfeed2.publish(pubBatt))
+            publishResults = false;
+          delay(10);
+        }
+      
+        if (publishResults)
+        {
+          Serial.println("Data published to Adafruit IO.");
+        } else {
+          Serial.println("FAILED to publish data to Adafruit IO!");
+        }
+       */
       
       rf95.waitPacketSent();
       Serial.println("Sent a reply");
@@ -96,4 +201,26 @@ void loop()
       Serial.println("Receive failed");
     }
   }
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect()
+{
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+  }
+  Serial.println("MQTT Connected!");
 }
